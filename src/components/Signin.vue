99c7c262a3990c6192fcc6ef125f4fe70266a957
Rudmanmrrod <rudmanmrrod@gmail.com>
2019-06-17 14:36:07 -0400

    Ajustados detalles de login

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,6 +1,7 @@
 <template>
   <b-container fluid>
     <h1>Sign In</h1>
+    <hr>
     <b-alert 
       variant="danger"
       v-model="error" 
@@ -8,21 +9,29 @@
     >
       {{error}}
     </b-alert>
-    <b-row>
-      <b-col sm="12">
-        <b-form-input 
+    <b-form>
+      <b-form-group
+        label="Correo:"
+        label-for="email"
+      >
+        <b-form-input
+          id="email"
           v-model="email" 
           placeholder="Ingresa tú correo"
         ></b-form-input>
-      </b-col>
-      <b-col sm="12">
+      </b-form-group>
+      <b-form-group
+        label="Contraseña:"
+        label-for="password"
+      >
         <b-form-input 
+          id="password"
           v-model="password" 
           placeholder="Ingresa tú contraseña"
           type="password"
         ></b-form-input>
-      </b-col>
-    </b-row>
+      </b-form-group>
+    </b-form>
     <b-button 
       variant="primary"
       @click="SignIn"
